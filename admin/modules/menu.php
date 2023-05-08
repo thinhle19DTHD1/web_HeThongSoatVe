e98d838331494a4db892025c6bed775a836b945a
Thinh <thinhletruongquoc66@gmail.com>
2023-05-08 18:40:00 +0700

    update

--- admin/modules/menu.php
+++ admin/modules/menu.php
@@ -3,9 +3,9 @@
             <ul class="list_menu">
                 <img src="" alt="">
                 <li><a href="index.php">Dashboard </a></li>
-                <li><a href="index.php?quanly=giohang">Campaign </a></li>
-                <li><a href="index.php?quanly=tintuc">Report </a></li>
-                <li><a href="index.php?quanly=lienhe">User Setting </a></li>
+                <li><a href="index.php?manage=campaign">Campaign </a></li>
+                <li><a href="index.php?manage=report">Report </a></li>
+                <li><a href="index.php?manage=usersetting">User Setting </a></li>
                 <!-- <a>Welcome Admin</a> -->
             </ul>
 </div>
