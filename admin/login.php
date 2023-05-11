bfd25fdcf7fa18709f5ac081ffe1a1bb183ecbcd
Thinh <thinhletruongquoc66@gmail.com>
2023-05-11 20:52:31 +0700

    update 11/5

---
+++ admin/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/styleadmin.css">
+    <title>Admin</title>
+</head>
+<body>
+<form method="post" action="#" class="box">
+        <H1>LOGIN</H1>
+        <input type="text" placeholder="Email"><br>
+        <input type="password" placeholder="Password"><br>
+        <input type="submit" value="ĐĂNG NHẬP">
+ </form>
+</body>
+</html>
--- admin/modules/menu.php
+++ admin/modules/menu.php
@@ -1,7 +1,7 @@
 
 <div class="menu"> 
             <ul class="list_menu">
-                <img name=""  src="../../images/logo.png">
+                <img src="../../images/logo.png">
                 <li><a href="index.php">Dashboard </a></li>
                 <li><a href="index.php?manage=campaign">Campaign </a></li>
                 <li><a href="index.php?manage=report">Report </a></li>
