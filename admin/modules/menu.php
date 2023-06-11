877912314117f2909c7f483a8e1f62fbedd0729b
Thinh <thinhletruongquoc66@gmail.com>
2023-06-11 13:31:37 +0700

    update

--- admin/modules/menu.php
+++ admin/modules/menu.php
@@ -5,7 +5,7 @@
                 <li><a href="index.php">Dashboard </a></li>
                 <li><a href="index.php?manage=campaign">Campaign </a></li>
                 <li><a href="index.php?manage=report">Report </a></li>
-                <li><a href="index.php?manage=usersetting">User Setting </a></li>
-                <!-- <a>Welcome Admin</a> -->
+                <li><a href="index.php?manage=usersetting">User Setting </a> </li>
+                <h2 style="margin-left: 1168px;margin-top:-40px;">Wellcome to admin</h2>
             </ul>
 </div>
