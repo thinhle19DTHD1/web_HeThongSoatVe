b02271a1fe4f9a2fab485e69c5ab7e614370ca46
Thinh <thinhletruongquoc66@gmail.com>
2023-06-14 01:25:13 +0700

    update

--- admin/modules/menu.php
+++ admin/modules/menu.php
@@ -1,11 +1,18 @@
 
 <div class="menu"> 
-            <ul class="list_menu">
-                <img src="../../images/logo.png">
-                <li><a href="index.php">Dashboard </a></li>
-                <li><a href="index.php?manage=campaign">Campaign </a></li>
-                <li><a href="index.php?manage=report">Report </a></li>
-                <li><a href="index.php?manage=usersetting">User Setting </a> </li>
-                <h2 style="margin-left: 1168px;margin-top:-40px;">Wellcome to admin</h2>
-            </ul>
+        <ul class="list_menu">
+            <img src="../../images/logo.png">
+            <li><a href="index.php">Dashboard </a></li>
+            <li><a href="index.php?manage=campaign">Campaign </a></li>
+            <li><a href="index.php?manage=report">Report </a></li>
+            <li><a href="index.php?manage=usersetting">User Setting </a> </li>
+
+            <?php 
+            if(isset($_SESSION['dangnhap'])){
+            ?>
+                <h2 style="margin-left: 1154px;margin-top:-40px;color:aliceblue;">Wellcome <?php echo '<span style="color:aliceblue">'.$_SESSION['dangnhap'].'</span>';?></h2>
+            <?php
+            }   
+            ?>
+         </ul>
 </div>
